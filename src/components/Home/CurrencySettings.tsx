4e3de7ead5c8de72535ba3b4f7dbec8c3e5cc353
Warano02 <carineteoi@gmail.com>
2025-09-11 11:08:48 +0100

    ccurency settings

--- src/components/Home/CurrencySettings.tsx
+++ src/components/Home/CurrencySettings.tsx
@@ -1,44 +1,38 @@
 import { useAppContext } from "@/hooks/useAppContext"
 import type { CurrencySettingsProps } from "@/types"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs"
+import { ScrollArea, ScrollBar } from "../ui/scroll-area"
 
 function CurrencySettings() {
-  const { setShowCurrencySettings } = useAppContext()
-  const currencyList=[
-    {
-      name:"United State dollar",
-      abr:"USD",
-      signe:"$"
-    },
-    {
-      name:"Australlian dollar",
-      abr:"AUD",
-      signe:"$"
-    },{
-      name:"Brazillian real",
-      abr:"BRL",
-      signe:"R$"
-    },{
-      name:"Bulgarian lev",
-      abr:"BGN",
-      
+  const { setShowCurrencySettings, currencyList, currency, setCurrency } = useAppContext()
 
-    }
-  ]
   return (
     <div className="fixed w-full h-full  bg-[#222222ab] z-10 flex justify-center items-center" onClick={() => setShowCurrencySettings(false)}>
       <div className="w-3/4 h-3/4 bg-white rounded-3xl p-5" onClick={(ev) => ev.stopPropagation()}>
         <Tabs defaultValue="currency">
-         <TabsList>
-          <TabsTrigger value="currency" className="cursor-pointer">Currency</TabsTrigger>
-          <TabsTrigger value="password" className="cursor-pointer">Password</TabsTrigger>
-        </TabsList>
+          <TabsList>
+            <TabsTrigger value="currency" className="cursor-pointer">Currency</TabsTrigger>
+            <TabsTrigger value="password" className="cursor-pointer">Password</TabsTrigger>
+          </TabsList>
 
           <TabsContent value="currency">
-            <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-2">Choose a currency</h1>
-            <div className="relative grid grid-cols-4 gap-2">
-              <Currency name="United State dollar" abr="USD" signe="$" isSelected={true} />
-            </div>
+            <h1 className="scroll-m-20 border-b pb-2 text-3xl  font-semibold tracking-tight first:mt-0 mb-2">Choose a currency</h1>
+            <ScrollArea className="w-full h-64">
+              <div className="grid grid-cols-4 gap-2 p-2 w-full h-full">
+                {currencyList.map(el => (
+                  <Currency
+                    key={el.abr}
+                    name={el.name}
+                    abr={el.abr}
+                    signe={el.signe}
+                    isSelected={currency === el.abr}
+                    onClick={() => setCurrency(el.abr)}
+                  />
+                ))}
+              </div>
+              <ScrollBar orientation="vertical"  />
+            </ScrollArea>
+
           </TabsContent>
         </Tabs>
 
@@ -48,11 +42,17 @@ function CurrencySettings() {
   )
 }
 
-function Currency({ name, abr, signe, isSelected }: CurrencySettingsProps) {
-  return <div className={`flex flex-col ${isSelected ? 'border' : ''} rounded w-auto p-1 cursor-pointer hover:bg-gray transition`}>
-    <span >{name} </span>
-    <span className="text-muted-foreground text-sm">{abr}- {signe} </span>
-  </div>
+function Currency({ name, abr, signe, isSelected, onClick }: CurrencySettingsProps) {
+  return (
+    <div
+      onClick={onClick}
+      className={`flex flex-col ${isSelected ? 'border' : ''} rounded p-2 cursor-pointer hover:bg-gray-100 transition w-[200px]`}
+    >
+      <span>{name}</span>
+      <span className="text-muted-foreground text-sm">{abr} - {signe}</span>
+    </div>
+  )
 }
+
 
 export default CurrencySettings
--- src/types/AppContextAndProvider.d.ts
+++ src/types/AppContextAndProvider.d.ts
@@ -3,13 +3,27 @@ export type User = {
   name: string;
   profile: string;
 };
+export type Currency = {
+  name: string
+  abr: string
+  signe: string
+  ratePerUSD: number
+}
 
 export type AppContextType = {
   user: User | undefined;
   setUser: React.Dispatch<React.SetStateAction<User | undefined>>;
+  currency: string;
+  setCurrency: React.Dispatch<React.SetStateAction<string>>;
   showCurrencySettings: boolean;
-  setShowCurrencySettings:React.Dispatch<React.SetStateAction<boolean>>;
-  
+  setShowCurrencySettings: React.Dispatch<React.SetStateAction<boolean>>;
+  currencyList: Currency[];
 };
 
-export type CurrencySettingsProps={name:string,abr:string,signe:string,isSelected:boolean}
+export type CurrencySettingsProps = {
+  name: string;
+  abr: string;
+  signe: string;
+  isSelected: boolean;
+  onClick: () => void;
+};
