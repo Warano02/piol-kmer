832a627343afbe32e205c56267fd4856362e1c92
Warano02 <carineteoi@gmail.com>
2025-09-17 18:53:31 +0100

    footer optimiser

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,93 +1,126 @@
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const groupsItems = [
+    {
+      title: "Support",
+      items: [
+        {
+          label: 'Help Center',
+          link: "/"
+        },
+        {
+          label: 'AirCover',
+          link: "/"
+        },
+        {
+          label: 'Safety information',
+          link: "/"
+        },
+        {
+          label: 'Cancellation options',
+          link: "/"
+        },
+        {
+          label: 'Report a neighborhood concern',
+          link: "/"
+        },
+      ]
+    },
+    {
+      title: "Community",
+      items: [
+        {
+          label: ' Airbnb.org: disaster relief housing',
+          link: "/"
+        },
+        {
+          label: 'Support Afghan refugees',
+          link: "/"
+        },
+        {
+          label: 'Combating discrimination',
+          link: "/"
+        },
+      ]
+    },
+    {
+      title: "Hosting",
+      items: [
+        {
+          label: 'Try hosting',
+          link: "/"
+        },
+        {
+          label: 'AirCover for Hosts',
+          link: "/"
+        },
+        {
+          label: 'Explore hosting resources',
+          link: "/"
+        },
+        {
+          label: 'Visit our community forum',
+          link: "/"
+        },
+        {
+          label: 'How to host responsibly',
+          link: "/"
+        },
+      ]
+    },
+    {
+      title: "Support",
+      items: [
+        {
+          label: 'Newsroom',
+          link: "/"
+        },
+        {
+          label: 'Learn about new features',
+          link: "/"
+        },
+        {
+          label: 'Letter from our founders',
+          link: "/"
+        },
+        {
+          label: 'Careers',
+          link: "/"
+        },
+        {
+          label: 'Investors',
+          link: "/"
+        },
+        {
+          label: 'Gift cards',
+          link: "/"
+        },
+      ]
+    }
+  ]
   return (
-    <footer className="bg-gray-100 border-t border-borderColor px-4">
+    <footer className="bg-[#F7F7F7] border-t border-borderColor px-4">
       <div className="max-w-[1200px] mx-auto py-10">
         <div className="flex flex-wrap gap-4">
-          <div className="my-6 lg:m-0 w-full md:w-[calc(100%/2-20px)] lg:w-[calc(100%/3-20px)] xl:w-[calc(100%/4-20px)]">
-            <h1 className="text-md font-semibold mb-4">Support</h1>
-            <ul>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Help Center
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                AirCover
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Safety information
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Supporting people with disabilities
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Cancellation options
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Our COVID-19 Response
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Report a neighborhood concern
-              </li>
-            </ul>
-          </div>
-          <div className="my-6 lg:m-0 w-full md:w-[calc(100%/2-20px)] lg:w-[calc(100%/3-20px)] xl:w-[calc(100%/4-20px)]">
-            <h1 className="text-md font-semibold mb-4">Community</h1>
-            <ul>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Airbnb.org: disaster relief housing
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Support Afghan refugees
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Combating discrimination
-              </li>
-            </ul>
-          </div>
-          <div className="my-6 lg:m-0 w-full md:w-[calc(100%/2-20px)] lg:w-[calc(100%/3-20px)] xl:w-[calc(100%/4-20px)]">
-            <h1 className="text-md font-semibold mb-4">Hosting</h1>
-            <ul>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Try hosting
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                AirCover for Hosts
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Explore hosting resources
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Visit our community forum
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                How to host responsibly
-              </li>
-            </ul>
-          </div>
-          <div className="my-6 lg:m-0 w-full md:w-[calc(100%/2-20px)] lg:w-[calc(100%/3-20px)] xl:w-[calc(100%/4-20px)]">
-            <h1 className="text-md font-semibold mb-4">Support</h1>
-            <ul>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Newsroom
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Learn about new features
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Letter from our founders
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Careers
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Investors
-              </li>
-              <li className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
-                Gift cards
-              </li>
-            </ul>
-          </div>
+          {groupsItems.map((el, i) => (
+
+            <div key={i} className="my-6 lg:m-0 w-full md:w-[calc(100%/2-20px)] lg:w-[calc(100%/3-20px)] xl:w-[calc(100%/4-20px)]">
+              <h1 className="text-md font-semibold mb-4">{el.title} </h1>
+              <ul>
+                {el.items.map((e, i) => (
+                  <li key={i} className="my-2 hover:underline cursor-pointer text-lightTextColor text-sm block py-1 md:p-0">
+                    <Link to={e.link}>{e.label} </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
+      </div>
+
+      <div className="w-full h-[75px] mt-2 border-t-[0.3px] border-[#cac7c779] flex justify-between items-center">
+
       </div>
     </footer>
   );
