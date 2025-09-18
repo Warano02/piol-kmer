5bc7752906a29588dd9ba873c74b235ec9dc5b0e
Warano02 <carineteoi@gmail.com>
2025-09-19 00:10:47 +0100

    image for 3

--- src/components/rooms/Gallery.tsx
+++ src/components/rooms/Gallery.tsx
@@ -10,46 +10,73 @@ export default function Gallery({ images, setShowHouseImages, setSelectedImage }
 
         <div className={`flex gap-2 w-full`}>
             {images.length === 1 && <TemplateFor1 urls={images} handleClick={handleClick} />}
-            {<TemplateFor2 handleClick={handleClick} urls={images} />}
+            {images.length === 2 && <TemplateFor2 handleClick={handleClick} urls={images} />}
+            {images.length === 3 && <TemplateFor3 handleClick={handleClick} urls={images} />}
             {images.length === 4 && <TemplateFor4 urls={images} handleClick={handleClick} />}
         </div>
     );
 }
 
+const TemplateFor3 = ({ handleClick, urls, width = "full" }: GalleryTemplateProps) => {
+    return (
+        <div className="w-full h-[330px] relative flex gap-1">
+            <div className="w-1/2 h-full image-cover" onClick={() => handleClick(urls[0])}>
+                <img
+                    src={urls[0]}
+                    alt="Gallery image 1"
+                    className={`w-${width} h-full object-cover cursor-pointer  transition-transform`}
+                />
+            </div>
+
+            <div className="w-1/2 h-full flex flex-col gap-1 overflow-hidden">
+                {
+                    urls.slice(1, 3).map((el, i) => (
+                        <span key={i} onClick={() => handleClick(el)} className="w-full h-1/2 image-cover">
+                            <img
+                                src={el}
+                                alt="Gallery image 1"
+                                className={`w-${width} h-full object-cover cursor-pointer  transition-transform`}
+                            />
+                        </span>
+                    ))
+                }
+
+            </div>
+        </div>
+    )
+}
+
 const TemplateFor2 = ({ handleClick, urls, width = "full" }: GalleryTemplateProps) => {
     return (
         <div className="w-full h-[320px] flex gap-2">
-            <span className="w-1/2">
+            <span className="w-1/2 image-cover" onClick={() => handleClick(urls[0])}>
                 <img
-                    onClick={() => handleClick(urls[0])}
                     src={urls[0]}
                     alt="Gallery image 1"
-                    className={`w-${width} h-full object-cover cursor-pointer hover:scale-105 transition-transform`}
+                    className={`w-${width} h-full object-cover cursor-pointer  transition-transform`}
                 />
             </span>
-            <span className="w-1/2">
+            <span className="w-1/2 image-cover" onClick={() => handleClick(urls[0])}>
                 <img
-                    onClick={() => handleClick(urls[0])}
                     src={urls[1]}
                     alt="Gallery image 1"
-                    className={`w-${width} h-full object-cover cursor-pointer hover:scale-105 transition-transform`}
+                    className={`w-${width} h-full object-cover cursor-pointer  transition-transform`}
                 />
             </span>
-           
+
         </div>
     )
 }
 
 const TemplateFor1 = ({ handleClick, urls, width = "full" }: GalleryTemplateProps) => {
     return (
-       <div className="flex gap-2 w-full h-[330px]">
-         <img
-            onClick={() => handleClick(urls[0])}
-            src={urls[0]}
-            alt="Gallery image 1"
-            className={`w-${width} h-full object-cover cursor-pointer hover:scale-105 transition-transform`}
-        />
-       </div>
+        <div className="flex gap-2 w-full h-[330px] image-cover" onClick={() => handleClick(urls[0])}>
+            <img
+                src={urls[0]}
+                alt="Gallery image 1"
+                className={`w-${width} h-full object-cover cursor-pointer  transition-transform`}
+            />
+        </div>
     )
 }
 
@@ -58,35 +85,32 @@ const TemplateFor1 = ({ handleClick, urls, width = "full" }: GalleryTemplateProp
 const TemplateFor4 = ({ handleClick, urls }: GalleryTemplateProps) => {
     return (
         <div className="flex gap-2 w-full h-[301px]">
-            <span className={`flex-1 overflow-hidden rounded-2xl`}>
+            <span className={`flex-1 overflow-hidden rounded-2xl image-cover`} onClick={() => handleClick(urls[0])}>
                 <img
-                    onClick={() => handleClick(urls[0])}
                     src={urls[0]}
                     alt="Gallery image 1"
-                    className="w-full h-full object-cover cursor-pointer hover:scale-105 transition-transform"
+                    className="w-full h-full object-cover cursor-pointer  transition-transform"
                 />
             </span>
 
 
             <div className="flex flex-1 flex-col gap-2">
                 {urls.slice(1, 3).map((img, i) => (
-                    <span key={i} className="flex-1 overflow-hidden rounded-2xl">
+                    <span key={i} className="flex-1 overflow-hidden rounded-2xl image-cover" onClick={() => handleClick(img)}>
                         <img
-                            onClick={() => handleClick(img)}
                             src={img}
                             alt={`Gallery image ${i + 2}`}
-                            className="w-full h-full object-cover cursor-pointer hover:scale-105 transition-transform"
+                            className="w-full h-full object-cover cursor-pointer  transition-transform"
                         />
                     </span>
                 ))}
             </div>
 
-            <span className="flex-1 overflow-hidden rounded-2xl">
+            <span className="flex-1 overflow-hidden rounded-2xl" onClick={() => handleClick(urls[3])}>
                 <img
-                    onClick={() => handleClick(urls[3])}
                     src={urls[3]}
                     alt="Gallery image 4"
-                    className="w-full h-full object-cover cursor-pointer hover:scale-105 transition-transform"
+                    className="w-full h-full object-cover cursor-pointer  transition-transform"
                 />
             </span>
         </div>
