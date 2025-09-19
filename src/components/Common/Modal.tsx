69216529d4317981e67a66247bc9958219a838c4
Warano02 <carineteoi@gmail.com>
2025-09-19 02:32:27 +0100

    start modal logique

--- src/components/Common/Modal.tsx
+++ src/components/Common/Modal.tsx
@@ -1,8 +1,16 @@
+import { useAppContext } from "@/hooks/useAppContext";
+import type { ModalProps } from "@/types/Modal"
 
-function Modal() {
+function Modal({ width = "" }: ModalProps) {
+  console.log(width);
+  const { setShowModal, showModal } = useAppContext()
+
   return (
-    <div>
-        
+    <div onClick={() => setShowModal(false)} className={`${showModal ? "fixed" : "hidden"}  top-0 left-0 w-full h-full z-60 bg-[#1312126b] p-10 select-none flex justify-center items-center ${showModal ? "opacity-100" : "opacity-0"}`} >
+      <div className="w-3xs lg:w-xl h-64 rounded-3xl bg-white z-65" onClick={(e) => e.stopPropagation()}>
+
+      </div>
+
     </div>
   )
 }
