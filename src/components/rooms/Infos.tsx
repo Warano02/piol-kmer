c00a2db4a5315af3452c3d1ad3c18fff74b1a70f
Warano02 <carineteoi@gmail.com>
2025-09-25 14:44:32 +0100

    add a reviewer

--- src/components/rooms/Infos.tsx
+++ src/components/rooms/Infos.tsx
@@ -1,12 +1,16 @@
 import type { Houses } from "@/types"
 import { useState } from "react";
-import { Times } from "../Icons";
+import { ImgLeft, ImgRight, Times } from "../Icons";
+import { useAppContext } from "@/hooks/useAppContext";
+import { Star } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function Infos({ House }: { House: Houses }) {
   const [amenitiesModal, setAmenitiesModal] = useState(false);
+  const { AppName } = useAppContext()
   return (
     <div className="w-full">
-      <div className="flex items-start sm:items-center justify-between flex-col sm:flex-row gap-4 pb-6">
+      <div className="flex items-start  justify-between flex-col  gap-1 pb-6">
         <h1 className="text-lg md:text-2xl mb-2 font-semibold h">
           {House.title}
         </h1>
@@ -18,6 +22,33 @@ function Infos({ House }: { House: Houses }) {
           {House.capacity?.bedrooms} bedrooms {" · "} {House.capacity?.bed}{" "}
           beds
         </div>
+        <Link to={""} className="w-full h-24 rounded-2xl border mt-8 flex justify-between items-center px-4">
+          <div className="flex gap-1 ">
+            <ImgLeft />
+            <h1 className="h text-xl max-w-[80px] mt-[-14px] text-center">Guest Favorite</h1>
+            <ImgRight />
+          </div>
+          <p className="flex-1 h px-4 text-center">
+            One of the most loved homes on {AppName}, according to guests
+          </p>
+          <div className="flex gap-1">
+            <div className="flex flex-col border-r pr-8">
+              <h1 className="h text-xl text-center">5.0 </h1>
+              <div className="flex">
+                {Array.from({ length: 5 }, ((_, i) => (
+                  <Star key={i} className="w-3" />
+                )))}
+              </div>
+            </div>
+            <div className="flex flex-col ml-4">
+              <h1 className="h text-xl">8</h1>
+              <h6 className="text-black-secondary text-[14px]">Reviews</h6>
+            </div>
+            <div>
+            </div>
+          </div>
+
+        </Link>
       </div>
       <div className="py-8 border-y border-gray">
         <p className="text-md leading-7 text-justify">
@@ -53,55 +84,55 @@ function Infos({ House }: { House: Houses }) {
         </button>
       </div>
       {amenitiesModal && (<div className="fixed w-screen h-screen inset-0 z-30 flex items-center justify-center">
-            <div
-              className="fixed top-0 left-0 w-full h-full z-30 bg-[#0000006b] bg-opacity-40"
-              onClick={() => setAmenitiesModal(false)}
-            ></div>
-            <div className="w-[50rem] h-full max-h-[calc(100vh-110px)] bg-white z-40 relative rounded-xl animation_primary">
-              <header className="flex items-center py-6 px-4 cursor-pointer">
-                <button onClick={() => setAmenitiesModal(false)} className="cursor-pointer">
-                  <Times />
-                </button>
-              </header>
-              <main className="h-full w-full max-h-[calc(100vh-180px)] pt-3 md:pt-6 overflow-auto px-4">
-                <h1 className="text-2xl font-semibold">What this place offers?</h1>
-                <div className="h-full w-full">
-                  {House.amenities?.map((e,i) => (
-                    <li
-                      className="w-full py-4 md:py-6 border-b border-borderColor flex gap-2 items-center my-2"
-                      key={i}
-                    >
-                      <img
-                        src={`/icons/${e.icon
-                          .toLowerCase()
-                          .trim()
-                          .replaceAll(" ", "_")}.png`}
-                        className="w-6 h-6"
-                        alt=""
-                      />
-                      {e.name}
-                    </li>
-                  ))}
-                  {House.amenities?.map((e,i) => (
-                    <li
-                      className="w-full py-6 border-b border-borderColor flex gap-2 items-center my-2"
-                      key={i}
-                    >
-                      <img
-                        src={`/icons/${e.icon
-                          .toLowerCase()
-                          .trim()
-                          .replaceAll(" ", "_")}.png`}
-                        className="w-6 h-6"
-                        alt=""
-                      />
-                      {e.name}
-                    </li>
-                  ))}
-                </div>
-              </main>
+        <div
+          className="fixed top-0 left-0 w-full h-full z-30 bg-[#0000006b] bg-opacity-40"
+          onClick={() => setAmenitiesModal(false)}
+        ></div>
+        <div className="w-[50rem] h-full max-h-[calc(100vh-110px)] bg-white z-40 relative rounded-xl animation_primary">
+          <header className="flex items-center py-6 px-4 cursor-pointer">
+            <button onClick={() => setAmenitiesModal(false)} className="cursor-pointer">
+              <Times />
+            </button>
+          </header>
+          <main className="h-full w-full max-h-[calc(100vh-180px)] pt-3 md:pt-6 overflow-auto px-4">
+            <h1 className="text-2xl font-semibold">What this place offers?</h1>
+            <div className="h-full w-full">
+              {House.amenities?.map((e, i) => (
+                <li
+                  className="w-full py-4 md:py-6 border-b border-borderColor flex gap-2 items-center my-2"
+                  key={i}
+                >
+                  <img
+                    src={`/icons/${e.icon
+                      .toLowerCase()
+                      .trim()
+                      .replaceAll(" ", "_")}.png`}
+                    className="w-6 h-6"
+                    alt=""
+                  />
+                  {e.name}
+                </li>
+              ))}
+              {House.amenities?.map((e, i) => (
+                <li
+                  className="w-full py-6 border-b border-borderColor flex gap-2 items-center my-2"
+                  key={i}
+                >
+                  <img
+                    src={`/icons/${e.icon
+                      .toLowerCase()
+                      .trim()
+                      .replaceAll(" ", "_")}.png`}
+                    className="w-6 h-6"
+                    alt=""
+                  />
+                  {e.name}
+                </li>
+              ))}
             </div>
-          </div>)}
+          </main>
+        </div>
+      </div>)}
 
     </div>
   )
