7af79eb5c0d32581a05d279ffa476d80f5c69c32
Warano02 <carineteoi@gmail.com>
2025-10-08 14:40:36 +0100

    add loading page

---
+++ usingnext/app/loading.tsx
@@ -0,0 +1,12 @@
+
+function loading() {
+  return (
+    <div className="w-full min-h-96 flex justify-center items-center">
+        <div className="w-12 h-12 rounded-full border border-gray-200 animate-spin">
+
+        </div>
+    </div>
+  )
+}
+
+export default loading
