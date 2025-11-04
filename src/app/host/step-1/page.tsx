3bc19c0dc2d352dfcdd9c023cd93bed6555e773f
Warano02 <carineteoi@gmail.com>
2025-11-04 15:30:15 +0100

    create layout for hoster

--- src/app/host/step-1/page.tsx
+++ src/app/host/step-1/page.tsx
@@ -1,34 +1,20 @@
 "use client"
 import { useAppContext } from "@/hooks/useAppContext"
-import { Svg } from "../page"
-import Progress from "@/components/Hosting/Progress"
 
 function page() {
-    const { AppName } = useAppContext()
     return (
-        <div className="w-full h-full relative">
-            <header className={`w-full h-22 flex px-4 items-center justify-between border `} >
-                <span>
-                    <Svg />
-                </span>
-                <button className="py-2 px-6 rounded border border-black hover:border-gray cursor-pointer font-semibold p">
-                    Exit
-                </button>
-            </header>
-            <div className="w-full h-[490px]  my-2 gap-4 flex flex-col md:flex-row px-2">
-                <div className="flex-1 h-full  flex justify-center px-8 flex-col gap-5 ">
-                    <h3 className="font-bold h text-2xl text-gray-700">Step 1</h3>
-                    <h1 className="text-5xl font-bold h">Tell us about your place</h1>
-                    <p className="text-xl p">In this step, we'll ask you wich type of property you have and if guest will book the entire place or just a room. Then let us know the location and how many guest can stay</p>
-                </div>
-                <div className="flex-1  ">
-                    <video autoPlay
-                        muted
-                        playsInline
-                        className="w-full h-full rounded-xl " src="https://stream.media.muscache.com/zFaydEaihX6LP01x8TSCl76WHblb01Z01RrFELxyCXoNek.mp4?v_q=high"></video>
-                </div>
+        <div className="w-full h-[470px]  my-2 gap-4 flex flex-col md:flex-row px-2">
+            <div className="flex-1 h-full  flex justify-center px-8 flex-col gap-5 ">
+                <h3 className="font-bold h text-2xl text-gray-700">Step 1</h3>
+                <h1 className="text-5xl font-bold h">Tell us about your place</h1>
+                <p className="text-xl p">In this step, we'll ask you wich type of property you have and if guest will book the entire place or just a room. Then let us know the location and how many guest can stay</p>
             </div>
-            <Progress />
+            <div className="flex-1  ">
+                <video autoPlay
+                    muted
+                    playsInline
+                    className="w-full h-full rounded-xl " src="https://stream.media.muscache.com/zFaydEaihX6LP01x8TSCl76WHblb01Z01RrFELxyCXoNek.mp4?v_q=high"></video>
+            </div>
         </div>
     )
 }
--- src/components/Hosting/Progress.tsx
+++ src/components/Hosting/Progress.tsx
@@ -3,7 +3,7 @@ function Progress() {
     const handleNextStep = () => alert("Yo next step")
     const handleBack = () => alert("Yo need to go back")
     return (
-        <section className="w-full h-[90px] fixed bottom-0">
+        <section className="w-full h-[80px] fixed bottom-0">
             <div className="w-full h-full relative  flex flex-col">
                 <div className="w-full h-[10px] flex relative">
                     <div className={`w-1/3 h-full bg-gray`}></div>
@@ -14,7 +14,9 @@ function Progress() {
                 </div>
                 <div className="w-full h-[80px] flex justify-between items-center px-8">
                     <button onClick={() => handleBack} className="px-8 py-4 rounded-2xl underline font-bold  h cursor-pointer">Back</button>
-                    <button onClick={() => handleNextStep} className="px-8 py-4 rounded-xl bg-black text-white text-2xl font-bold cursor-pointer h">Next</button>
+                    <button onClick={() => handleNextStep} className="px-6 py-3 rounded-xl bg-black text-white text-xl font-bold cursor-pointer h">
+                        Next
+                    </button>
                 </div>
             </div>
         </section>
