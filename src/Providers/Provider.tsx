28d5cbd02b2f2609625c9d4ab5e0e7b0c662ad48
Warano02 <carineteoi@gmail.com>
2025-12-11 07:09:22 +0100

    add currency provider

--- src/Providers/Provider.tsx
+++ src/Providers/Provider.tsx
@@ -2,13 +2,16 @@
 import React from "react"
 import { AppProvider } from "./AppProvider"
 import { ThemeProvider } from "./ThemeProvider"
+import CurrencyProvider from "./CurrencyProvider"
 
-function Provider({children}:{children:React.ReactNode}) {
+function Provider({ children }: { children: React.ReactNode }) {
   return (
     <AppProvider>
-<ThemeProvider>
-    {children}
-</ThemeProvider>
+      <CurrencyProvider>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </CurrencyProvider>
     </AppProvider>
   )
 }
