c5dac392308d50f28805e547d230445ef54d6c9f
Warano02 <carineteoi@gmail.com>
2026-02-08 19:37:58 +0100

    start adding ressource files

---
+++ app/resources/hosting-home/how-to-pick-your-booking-settings/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const metadata = {
+    title: "Pick your booking settings - PiolKmer",
+    description: "Become a Host and Rent Out Your Room, House or Apartment on PiolKmer",
+}
+
+function page() {
+  return (
+    <div>hOW TO PICK YOUR BOOKING SETTINGS https://www.airbnb.com/resources/hosting-homes/a/how-to-pick-your-booking-settings-619</div>
+  )
+}
+
+export default page
---
+++ app/resources/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const metadata = {
+    title: "Resources Center - PiolKmer",
+    description: "Learn about becoming a Host and renting out your Room, House or Apartment on PiolKmer",
+}
+
+function page() {
+  return (
+    <div>page https://www.airbnb.com/resources</div>
+  )
+}
+
+export default page
